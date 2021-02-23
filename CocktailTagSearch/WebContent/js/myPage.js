fedab285a51ea37dc25e2beffe4db41a4c55c5e9
Mmoka03 <l55922477@gmail.com>
2021-02-23 14:58:40 +0900

    추가: 마이페이지 로그인 시간 만료
    마이페이지에서 세션의 만료가 확인되면 로그아웃과 동시에 메인페이지로 이동합니다

--- CocktailTagSearch/WebContent/js/myPage.js
+++ CocktailTagSearch/WebContent/js/myPage.js
@@ -5,33 +5,35 @@ function getUserData() {
     type:"post",
 	url:"http://localhost:8090/CocktailTagSearch/FavoriteData",
 	dataType: "json",
-	error : function(error) {
-        console.log(error);
-    },
     success: function(data) {
-	
-	if(data != null) {
+	  if(data != null) {
+	    if(data.user != null) {
 	      $('.myPageName').text(data.user.name);
 	      $('.myPageId').text(data.user.login_id);
-		  $.each(data.cocktail, function(index, item) {
-			var cocktail = $('<div/>');
-			cocktail.addClass('myPageCocktails');
-			cocktail.attr('desc', item.name);
-			cocktail.text(item.name);
-			cocktail.on('click', function() {
-			  var url = "Cocktail_post.jsp?id="+item.id;
-			  $(location).attr('href', url);
-			});
-			$('#myPageCocktailContents').append(cocktail);
-		  });
-		  $.each(data.tag, function(index, item) {
-			$('#myPageTagContents').append($('<div/>', {
-		      class: "myPageTags",
-			  desc: item.desc,
-		      text: item.name
-		    }));
-		  });
+	      $.each(data.cocktail, function(index, item) {
+	        var cocktail = $('<div/>');
+	        cocktail.addClass('myPageCocktails');
+		    cocktail.attr('desc', item.name);
+		    cocktail.text(item.name);
+		    cocktail.on('click', function() {
+	          var url = "Cocktail_post.jsp?id="+item.id;
+		      $(location).attr('href', url);
+		    });
+	          $('#myPageCocktailContents').append(cocktail);
+          });
+          $.each(data.tag, function(index, item) {
+	        $('#myPageTagContents').append($('<div/>', {
+              class: "myPageTags",
+	          desc: item.desc,
+              text: item.name
+            }));
+          }); 
+	    } else {
+	      alert("로그인 시간이 만료되었습니다.");
+		  logout();
+		  location.href = "index.html";
 	    }
+	  }
 	}
 	
   });
